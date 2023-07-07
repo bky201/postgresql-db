b281e6e18da1ae57781f9524af7231ccebc83a13
bek <bekrye@gmail.com>
2023-07-07 13:49:56 +0200

    Add categories

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+
 from taskmanager import app
 
 
---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav innitialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
